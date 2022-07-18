801e775aebc4fc79c0cd3243b7c1ed0fbca9a4e7
Ali <ali.baqeri.yazdabady@gmail.com>
2022-07-18 10:51:01 +0430

    1401-4-27, p1

--- lib/dateSection/calendarTools.dart
+++ lib/dateSection/calendarTools.dart
@@ -1,88 +1,19 @@
 
-class CalendarType {
-  TypeOfCalendar _type = TypeOfCalendar.gregorian;
-
-  CalendarType.gregorian();
-
-  CalendarType.byName(String? name){
-    if(name == null){
-      _type = TypeOfCalendar.unKnow;
-    }
-
-    if(name == 'solar-hijri') {
-      _type = TypeOfCalendar.solarHijri;
-    }
-    else if(name == 'gregorian') {
-      _type = TypeOfCalendar.gregorian;
-    }
-
-    _type = TypeOfCalendar.unKnow;
-  }
-
-  CalendarType.byType(TypeOfCalendar t){
-    _type = t;
-  }
-
-  String get name {
-    if(_type == TypeOfCalendar.gregorian) {
-      return 'gregorian';
-    }
-
-    if(_type == TypeOfCalendar.solarHijri) {
-      return 'solar-hijri';
-    }
-
-    return 'unknown';
-  }
-
-  TypeOfCalendar get type {
-    return _type;
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if(other is! CalendarType) {
-      return false;
-    }
-
-    return (other.type == type);
-  }
-
-  @override
-  int get hashCode => super.hashCode;
-}
-///===========================================================================================================
-enum TypeOfCalendar {
-  gregorian,
+enum CalendarType {
   solarHijri,
+  gregorian,
   unKnow,
 }
 
-extension TypeOfCalendarExtension on TypeOfCalendar {
+extension TypeOfCalendarExtension on CalendarType {
 
-  String get name {
-    switch (this) {
-      case TypeOfCalendar.gregorian:
-        return 'gregorian';
-      case TypeOfCalendar.solarHijri:
-        return 'solar-hijri';
-      default:
-        return 'unknown';
-    }
-  }
-
-  TypeOfCalendar byName(String? name){
-    if(name == null){
-      return TypeOfCalendar.gregorian;
+  static CalendarType fromName(String? name){
+    for(final t in CalendarType.values){
+      if(t.name == name){
+        return t;
+      }
     }
 
-    if(name == 'solar-hijri') {
-      return TypeOfCalendar.solarHijri;
-    }
-    else if(name == 'gregorian'){
-      return TypeOfCalendar.gregorian;
-    }
-
-    return TypeOfCalendar.unKnow;
+    return CalendarType.unKnow;
   }
 }
