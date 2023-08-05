e8c54bdfca7510a4fdb468b71fc127f979757e8a
ali bagheri <bagheri_a@nicode.org>
2023-08-05 10:16:41 +0330

    1402-05-14, c1 plugin

---
+++ android/src/main/java/ir/iris/android_tools/NotificationHelper.java
@@ -0,0 +1,51 @@
+package ir.iris.android_tools;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+//import androidx.core.app.RemoteInput;
+//import android.app.PendingIntent;
+//import android.content.Intent;
+
+public class NotificationHelper {
+    static private final String CHANNEL_ID = "c_01";
+    static private final String CHANNEL_NAME = "flutter_app";
+    static private final int NOTIFICATION_ID = 101;
+
+    static void createChannel(Context context) {
+        NotificationManagerCompat manager = NotificationManagerCompat.from(context);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
+            channel.setDescription("channel");
+            channel.enableVibration(false);
+            channel.enableLights(true);
+
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    static void showNotification(Context context, String msg) {
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentTitle(msg)
+                .setColor(Color.BLUE)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .build();
+
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification);
+    }
+
+    static void dismissNotification(Context context, int notificationId) {
+        NotificationManagerCompat.from(context).cancel(null, notificationId);
+    }
+}
