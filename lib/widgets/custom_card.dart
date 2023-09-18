cd5975194d58609bc8689269217b60bfc32e3b50
ali bagheri <bagheri_a@nicode.org>
2023-09-18 10:27:33 +0330

    1402-06-27, c1, work

---
+++ lib/widgets/custom_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final Widget child;
+  final EdgeInsets? padding;
+  final Color? color;
+  final double? radius;
+  final BoxBorder? border;
+  final BorderRadius? borderRadius;
+
+  const CustomCard({
+    required this.child,
+    this.padding,
+    this.radius,
+    this.color,
+    this.border,
+    this.borderRadius,
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(radius?? 8),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: border,
+              borderRadius: borderRadius?? BorderRadius.circular(radius?? 8),
+              color: color?? Colors.white
+          ),
+          child: Padding(
+            padding: padding?? EdgeInsets.zero,
+            child: child,
+          ),
+        )
+    );
+  }
+}
