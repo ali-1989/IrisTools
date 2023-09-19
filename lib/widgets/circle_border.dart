318d6b650c611afad0e2f425209a05fedb4edf0f
ali bagheri <bagheri_a@nicode.org>
2023-09-19 15:35:06 +0330

    1402-06-28, c2, work

---
+++ lib/widgets/circle_border.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CircleBorder extends StatelessWidget {
+  final Widget child;
+  final Color borderColor;
+  final double width;
+  final EdgeInsets padding;
+
+  const CircleBorder({
+    required this.child,
+    required this.borderColor,
+    required this.width,
+    this.padding = const EdgeInsets.all(3),
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: borderColor, width: width),
+        ),
+      child: Padding(
+        padding: padding,
+        child: child,
+      ),
+    );
+  }
+}
