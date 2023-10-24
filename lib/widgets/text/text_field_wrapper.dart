f0c4f24e5afdaf5ebe837186f34b452f5011d54d
ali-1989 <ali.baqeri.yazdabady@gmail.com>
2023-10-24 12:48:15 +0330

    1402-08-02, c4, home

--- lib/widgets/text/text_field_wrapper.dart
+++ lib/widgets/text/text_field_wrapper.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-typedef TextFieldMessageBuilder = Widget? Function(BuildContext context, TextEditingController controller, TextField textField);
+typedef TextFieldMessageBuilder = Widget? Function(BuildContext context, TextEditingController controller);
+typedef TextFieldBuilder = Widget Function(BuildContext context, TextEditingController controller, String? error);
 
 class TextFieldWrapper extends StatefulWidget {
-  final TextField child;
+  final TextFieldBuilder builder;
   final TextEditingController controller;
   final TextFieldMessageBuilder messageBuilder;
 
   // ignore: prefer_const_constructors_in_immutables
-  TextFieldWrapper({super.key, required this.child, required this.controller, required this.messageBuilder});
+  TextFieldWrapper({super.key, required this.builder, required this.controller, required this.messageBuilder});
 
   @override
   State<StatefulWidget> createState() {
@@ -20,19 +21,23 @@ class TextFieldWrapperState extends State<TextFieldWrapper> {
 
   @override
   Widget build(BuildContext context) {
+    final message = genMessage();
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        widget.child,
-        genMessage(),
+        widget.builder(context, widget.controller, genError(message)),
+        message?? SizedBox(),
       ],
     );
   }
 
-  Widget genMessage(){
-    final res = widget.messageBuilder.call(context, widget.controller, widget.child);
+  Widget? genMessage(){
+    return widget.messageBuilder.call(context, widget.controller);
+  }
 
-    return res ?? const SizedBox();
+  String? genError(dynamic message){
+    return message == null? null : '';
   }
 }
